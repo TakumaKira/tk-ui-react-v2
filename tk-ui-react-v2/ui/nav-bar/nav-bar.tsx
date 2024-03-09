9c6214b1ed42be50d858855d0a0dfff8b97c2657
Takuma Kira <takuma@kirakiraworx.com>
2024-03-09 18:30:31 +0900

    [dev]: Recover all the changes from v1

--- tk-ui-react-v2/ui/nav-bar/nav-bar.tsx
+++ tk-ui-react-v2/ui/nav-bar/nav-bar.tsx
@@ -1,16 +1,21 @@
-import type { ReactNode } from 'react';
+import classNames from 'classnames';
+import styles from './nav-bar.module.scss';
 
 export type NavBarProps = {
   /**
-   * sets the component children.
+   * sets the title.
    */
-  children?: ReactNode;
+  children?: string;
+  height?: number;
+  titleStyles?: CSSModuleClasses['string'];
 };
 
-export function NavBar({ children }: NavBarProps) {
+export function NavBar({ children, height = 64, titleStyles = styles.titleDefault }: NavBarProps) {
   return (
-    <div>
-      {children}
-    </div>
+    <nav className={classNames(styles.navBar)} style={{ height }}>
+      <header className={classNames(titleStyles)}>
+        {children}
+      </header>
+    </nav>
   );
 }
