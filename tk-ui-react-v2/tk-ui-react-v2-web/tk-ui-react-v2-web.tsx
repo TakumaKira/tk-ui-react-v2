3224e73116a52f85cb2b6addd7616c46b4e67e5b
Takuma Kira <takuma@kirakiraworx.com>
2024-03-09 19:53:33 +0900

    [fix]: Use fixed NavBar component

--- tk-ui-react-v2/tk-ui-react-v2-web/tk-ui-react-v2-web.tsx
+++ tk-ui-react-v2/tk-ui-react-v2-web/tk-ui-react-v2-web.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import { Colors, colorsTokensDark } from '@takumakira-individual/tk-ui-react-v2.themes.colors';
 import { useThemeSwitcher, Theme } from '@takumakira-individual/tk-ui-react-v2.hooks.use-theme-switcher';
-// import { NavBar } from '@takumakira-individual/tk-ui-react-v2.ui.nav-bar';
+import { NavBar } from '@takumakira-individual/tk-ui-react-v2.ui.nav-bar';
 import styles from './tk-ui-react-v2-web.module.scss';
 
 export function TkUiReactV2Web() {
@@ -10,7 +10,7 @@ export function TkUiReactV2Web() {
   return (
     <Colors overrides={theme === Theme.DARK ? colorsTokensDark : undefined}>
       <div className={classNames(styles.container)}>
-        {/* <NavBar>Title</NavBar> */}
+        <NavBar titleStyles=''>Title</NavBar>
         <main className={classNames(styles.main)}>
           <div className={classNames(styles.content)}>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem expedita, ducimus, repellat obcaecati similique tempore dolores inventore asperiores cumque temporibus praesentium maxime ipsum cum ut rem sint at doloribus ad.</p>
