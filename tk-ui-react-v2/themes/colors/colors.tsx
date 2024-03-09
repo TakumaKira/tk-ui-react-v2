0106d7377075134e2e2d33e022cf1e2d9987a02b
Takuma Kira <takuma@kirakiraworx.com>
2024-03-09 21:57:55 +0900

    [fix]: Use css to work in node_modules directory

--- tk-ui-react-v2/themes/colors/colors.tsx
+++ tk-ui-react-v2/themes/colors/colors.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
 import { ColorsProvider } from './colors-provider.js';
-import styles from './colors.module.scss';
+import styles from './colors.module.css';
 import { ColorsSchema } from './colors-tokens.js';
 
 export type ColorsProps = {
