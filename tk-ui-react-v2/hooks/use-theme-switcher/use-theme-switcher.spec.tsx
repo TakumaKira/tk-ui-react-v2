3cb5353e49795b4bb5601f44060788ff8045c3d3
Takuma Kira <takuma@kirakiraworx.com>
2024-03-10 02:43:57 +0900

    [fix]: Fix test

--- tk-ui-react-v2/hooks/use-theme-switcher/use-theme-switcher.spec.tsx
+++ tk-ui-react-v2/hooks/use-theme-switcher/use-theme-switcher.spec.tsx
@@ -3,7 +3,7 @@ import { renderHook } from '@testing-library/react';
 import { useThemeSwitcher, Theme } from './use-theme-switcher.js';
 
 it('should increment the counter', () => {
-  const { result } = renderHook(() => useThemeSwitcher());
+  const { result } = renderHook(() => useThemeSwitcher(window));
 
   expect(result.current).toBe(Theme.LIGHT);
 })
