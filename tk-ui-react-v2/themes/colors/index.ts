3a6a4bfc5862996fe2c2fcf1fc81ae40e04fa0d0
Takuma Kira <takuma@kirakiraworx.com>
2024-03-09 18:33:38 +0900

    [dev]: Recover all the changes on the sample app from v1

--- tk-ui-react-v2/themes/colors/index.ts
+++ tk-ui-react-v2/themes/colors/index.ts
@@ -2,3 +2,5 @@ export { Colors } from "./colors.js";
 export { useTheme } from "./colors-provider.js";
 export type { ColorsProps } from "./colors.js";
 export type { ColorsSchema } from "./colors-tokens.js";
+export { colorsTokens } from "./colors-tokens.js";
+export { colorsTokensDark } from "./colors-tokens-dark.js";
