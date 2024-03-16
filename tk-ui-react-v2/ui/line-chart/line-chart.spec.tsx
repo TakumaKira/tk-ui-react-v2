975bccbbc996d2226dd068d5787c137de5b312ff
Takuma Kira <takuma@kirakiraworx.com>
2024-03-16 18:09:43 +0900

    [test]: Fix LineChart test

--- tk-ui-react-v2/ui/line-chart/line-chart.spec.tsx
+++ tk-ui-react-v2/ui/line-chart/line-chart.spec.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BasicLineChart } from './line-chart.composition.js';
 
-it('should render the correct text', () => {
-  const { getByText } = render(<BasicLineChart />);
-  const rendered = getByText('hello world!');
-  expect(rendered).toBeTruthy();
+it('should render the canvas', () => {
+  const result = render(<BasicLineChart />);
+  expect(result.baseElement.children[0].children[0].tagName).toBe('CANVAS');
 });
