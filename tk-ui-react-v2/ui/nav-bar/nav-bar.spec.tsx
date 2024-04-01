e7876ac0e58926cf73c26a15e0cd9f7043c92ca3
Takuma Kira <takuma@kirakiraworx.com>
2024-04-01 09:52:47 +0900

    [refactor]: Define NavBar clearly

--- tk-ui-react-v2/ui/nav-bar/nav-bar.spec.tsx
+++ tk-ui-react-v2/ui/nav-bar/nav-bar.spec.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import { BasicNavBar } from './nav-bar.composition.js';
+import { NavBar } from './nav-bar.js';
 
 it('should render the correct text', () => {
-  const { getByText } = render(<BasicNavBar />);
-  const rendered = getByText('hello world!');
+  const { getByText } = render(<NavBar>Test</NavBar>);
+  const rendered = getByText('Test');
   expect(rendered).toBeTruthy();
 });
+
+/**
+ * Ideally, it would be better if css values were tested, but Jest is not for the purpose.
+ * At least we can confirm the same functionalities are working as tried to be tested below on `Composition` section of [the component page](https://bit.cloud/takumakira-individual/tk-ui-react-v2/themes/colors).
+ */
+
+it.skip('should render in the configured height', () => {
+  const { getByText } = render(<NavBar height={48}>Test</NavBar>);
+  const rendered = getByText('Test');
+  expect(rendered.style.height).toBe(48);
+})
