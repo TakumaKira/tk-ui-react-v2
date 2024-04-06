8bbe9339c68bc092a69813ee78f8d62ac766594e
Takuma Kira <takuma@kirakiraworx.com>
2024-04-07 01:22:16 +0900

    [fix]: Fix tk-ui-react-v2-web

--- tk-ui-react-v2/tk-ui-react-v2-web/tk-ui-react-v2-web.tsx
+++ tk-ui-react-v2/tk-ui-react-v2-web/tk-ui-react-v2-web.tsx
@@ -5,10 +5,10 @@ import { useThemeSwitcher, Theme } from '@takumakira-individual/tk-ui-react-v2.h
 import { NavBar } from '@takumakira-individual/tk-ui-react-v2.ui.nav-bar';
 import { LineChart, LineChartProps, getColor } from '@takumakira-individual/tk-ui-react-v2.ui.line-chart';
 import { ApiQueryClientProvider, useApi } from '@takumakira-individual/tk-ui-react-v2.hooks.use-api';
-import { JobTrendChart } from '@takumakira-individual/tk-ui-react-v2.ui.job-trend-chart';
+import { JobTrendChart, Result } from '@takumakira-individual/tk-ui-react-v2.ui.job-trend-chart';
 import styles from './tk-ui-react-v2-web.module.scss';
 
-const getPosts = (postId: number) => () => fetch(`https://jsonplaceholder.typicode.com/posts/${postId ?? ''}`).then(res => res.json())
+const getPosts = (postId: number) => () => fetch(`https://jsonplaceholder.typicode.com/posts/${postId ?? ''}`)
 function PostViewer() {
   const [postId, setPostId] = useState<number>(1)
   const { isLoading, data } = useApi(['post', { postId }], getPosts(postId))
@@ -26,7 +26,7 @@ function PostViewer() {
   )
 }
 
-const getTemperature = (lat: number, lon: number, tz: string) => () => fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&timezone=${tz}&past_days=3&forecast_days=3`).then(res => res.json())
+const getTemperature = (lat: number, lon: number, tz: string) => () => fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&timezone=${tz}&past_days=3&forecast_days=3`)
 const getTokyoTemperature = getTemperature(35.6895, 139.6917, 'Asia%2FTokyo')
 const getBerlinTemperature = getTemperature(52.5244, 13.4105, 'Europe%2FBerlin')
 function ChartViewer() {
@@ -64,6 +64,26 @@ function ChartViewer() {
   return <LineChart data={lineChartData} />
 }
 
+type JobTrendData = { job_title: string, job_location: string, count: number }
+const jobTrendDataValidator = (data: any): JobTrendData => {
+  if (typeof data?.job_title !== 'string') throw new Error('job_title must be a string')
+  if (typeof data?.job_location !== 'string') throw new Error('job_location must be a string')
+  if (typeof data?.count !== 'number') throw new Error('count must be a number')
+  return { job_title: data.job_title, job_location: data.job_location, count: data?.count }
+}
+const labelGetter = (data: Result<JobTrendData>) => `${data.job_title}/${data.job_location}`
+const countGetter = (data: Result<JobTrendData>) => data.count
+function JobTrendViewer() {
+  return (
+    <JobTrendChart
+      jobTrendDataApiEndpoint='http://localhost:5000'
+      jobTrendDataValidator={jobTrendDataValidator}
+      labelGetter={labelGetter}
+      countGetter={countGetter}
+    />
+  )
+}
+
 export function TkUiReactV2Web() {
   const { theme } = useThemeSwitcher()
   return (
@@ -75,7 +95,7 @@ export function TkUiReactV2Web() {
             <div className={classNames(styles.content)}>
               <PostViewer />
               <ChartViewer />
-              <JobTrendChart />
+              <JobTrendViewer />
             </div>
           </main>
         </div>
