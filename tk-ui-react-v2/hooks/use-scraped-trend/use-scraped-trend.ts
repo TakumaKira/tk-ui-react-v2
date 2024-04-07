2cf66da1e7b3f1dffe1eaf2684ded787235f1d9c
Takuma Kira <takuma@kirakiraworx.com>
2024-04-08 00:09:07 +0900

    [fix]: Remove @acme/acme.testing.mock-provider from dependency as it does not work well with Next.js

--- tk-ui-react-v2/hooks/use-scraped-trend/use-scraped-trend.ts
+++ tk-ui-react-v2/hooks/use-scraped-trend/use-scraped-trend.ts
@@ -1,15 +1,12 @@
 import { useMemo } from 'react';
 import { useApi } from '@takumakira-individual/tk-ui-react-v2.hooks.use-api';
-import { useIsMock } from '@acme/acme.testing.mock-provider';
 import { ScrapedTrend, mockPlainScrapedTrend } from '@takumakira-individual/tk-ui-react-v2.entities.scraped-trend';
 
 /**
  * A useScrapedTrend React hook.
  */
-export function useScrapedTrend<DataTypeWithoutDate extends Record<string, any>>(scrapedTrendResultApiEndpoint: string, validator: (data: any) => DataTypeWithoutDate): undefined | null | ScrapedTrend<DataTypeWithoutDate>[] {
-  const isMock = useIsMock();
-
-  const { isLoading, isError, data } = useApi(['scrapedTrend'], () => isMock ? Promise.resolve(new Response(JSON.stringify(mockPlainScrapedTrend()))) : fetch(scrapedTrendResultApiEndpoint))
+export function useScrapedTrend<DataTypeWithoutDate extends Record<string, any>>(scrapedTrendResultApiEndpoint: string, validator: (data: any) => DataTypeWithoutDate, useMockData?: boolean): undefined | null | ScrapedTrend<DataTypeWithoutDate>[] {
+  const { isLoading, isError, data } = useApi(['scrapedTrend'], () => useMockData ? Promise.resolve(new Response(JSON.stringify(mockPlainScrapedTrend()))) : fetch(scrapedTrendResultApiEndpoint))
   const validatedScrapedTrend = useMemo<ScrapedTrend<DataTypeWithoutDate>[]>(() => {
     if (isLoading) {
       return undefined
